bcc5a2e5b4f636edad279c327c049bd55ba6a60b
Tom Needham <06needhamt@gmail.com>
2015-08-10 19:40:08 +0100

    unstable

---
+++ Museum-App-iOS/AncientWorldContentController.swift
@@ -0,0 +1,28 @@
+//
+//  AncientWorldContentController.swift
+//  Museum-App-iOS
+//
+//  Created by THOMAS NEEDHAM on 10/08/2015.
+//  Copyright (c) 2015 THOMAS NEEDHAM. All rights reserved.
+//
+
+import UIKit
+
+class AncientWorldContentController: UIViewController {
+
+    @IBOutlet weak var AvailableTrailPicker: UIPickerView!
+    @IBOutlet weak var Background: UIImageView!
+    @IBOutlet weak var MapImage: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
---
+++ Museum-App-iOS/WorldCulturesContentController.swift
@@ -0,0 +1,25 @@
+//
+//  WorldCulturesContentController.swift
+//  Museum-App-iOS
+//
+//  Created by THOMAS NEEDHAM on 10/08/2015.
+//  Copyright (c) 2015 THOMAS NEEDHAM. All rights reserved.
+//
+
+import UIKit
+
+class WorldCulturesContentController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
